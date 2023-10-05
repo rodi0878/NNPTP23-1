e71fe665dc642a8d8bfd578c9916ffe802fca0d3
matthewpatthew <matej.patek.chot@seznam.cz>
2023-10-06 01:34:32 +0200

    Com1
    První verze, rozdělení na třídy a zajištěna funkcionalita

---
+++ NNPTPZ1/ComplexNumber.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace NNPTPZ1
+{
+
+    namespace Mathematics
+    {
+        public class ComplexNumber
+        {
+            public double Real { get; set; }
+            public float Imaginary { get; set; }
+
+            public ComplexNumber(double Real, float Imaginary)
+            {
+                this.Real = Real;
+                this.Imaginary = Imaginary;
+            }
+
+            public readonly static ComplexNumber Zero = new ComplexNumber(0, 0);
+            public override bool Equals(object obj)
+            {
+                if (obj is ComplexNumber)
+                {
+                    ComplexNumber complexNumber = obj as ComplexNumber;
+                    return complexNumber.Real == Real && complexNumber.Imaginary == Imaginary;
+                }
+                return base.Equals(obj);
+            }
+            public ComplexNumber Multiply(ComplexNumber complexNumberTwo)
+            {
+                ComplexNumber complexNumberOne = this;
+                double realPart = complexNumberOne.Real * complexNumberTwo.Real - complexNumberOne.Imaginary * complexNumberTwo.Imaginary;
+                float imaginaryPart = (float)(complexNumberOne.Real * complexNumberTwo.Imaginary + complexNumberOne.Imaginary * complexNumberTwo.Real);
+                return new ComplexNumber(realPart, imaginaryPart);
+            }
+            public double GetAbsoluteValue()
+            {
+                return Math.Sqrt( Real * Real + Imaginary * Imaginary);
+            }
+
+            public ComplexNumber Add(ComplexNumber complexNumberTwo)
+            {
+                ComplexNumber complexNumberOne = this;
+                double realPart = complexNumberOne.Real + complexNumberTwo.Real;
+                float imaginaryPart = complexNumberOne.Imaginary + complexNumberTwo.Imaginary;
+                return new ComplexNumber(realPart, imaginaryPart);
+            }
+            public double GetAngleInDegrees()
+            {
+                return Math.Atan(Imaginary / Real);
+            }
+            public ComplexNumber Subtract(ComplexNumber complexNumberTwo)
+            {
+                ComplexNumber complexNumberOne = this;
+                double realPart = complexNumberOne.Real - complexNumberTwo.Real;
+                float imaginaryPart = complexNumberOne.Imaginary - complexNumberTwo.Imaginary;
+                return new ComplexNumber(realPart, imaginaryPart);
+            }
+            public override string ToString()
+            {
+                return $"({Real} + {Imaginary}i)";
+            }
+
+            internal ComplexNumber Divide(ComplexNumber complexNumberTwo)
+            {
+                // (aRe + aIm*i) / (bRe + bIm*i)
+                // ((aRe + aIm*i) * (bRe - bIm*i)) / ((bRe + bIm*i) * (bRe - bIm*i))
+                //  bRe*bRe - bIm*bIm*i*i
+                ComplexNumber tmp = Multiply(new ComplexNumber() { Real = complexNumberTwo.Real, Imaginary = -complexNumberTwo.Imaginary });
+                var tmp2 = complexNumberTwo.Real * complexNumberTwo.Real + complexNumberTwo.Imaginary * complexNumberTwo.Imaginary;
+
+                return new ComplexNumber()
+                {
+                    Real = tmp.Real / tmp2,
+                    Imaginary = (float)(tmp.Imaginary / tmp2)
+                };
+            }
+        }
+    }
+}
---
+++ NNPTPZ1/Polynomial.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+
+namespace NNPTPZ1
+{
+
+    namespace Mathematics
+    {
+        public class Polynomial
+        {
+            /// <summary>
+            /// Coe
+            /// </summary>
+            public List<ComplexNumber> Coefficients { get; set; }
+
+            /// <summary>
+            /// Constructor
+            /// </summary>
+            public Polynomial() => Coefficients = new List<ComplexNumber>();
+
+            public void Add(ComplexNumber coefficient) =>
+                Coefficients.Add(coefficient);
+
+            /// <summary>
+            /// Derives this polynomial and creates new one
+            /// </summary>
+            /// <returns>Derivated polynomial</returns>
+            public Polynomial Derive()
+            {
+                Polynomial p = new Polynomial();
+                for (int i = 1; i < Coefficients.Count; i++)
+                {
+                    p.Coefficients.Add(Coefficients[i].Multiply(new ComplexNumber() { Real = i }));
+                }
+
+                return p;
+            }
+
+            /// <summary>
+            /// Evaluates polynomial at given point
+            /// </summary>
+            /// <param name="x">point of evaluation</param>
+            /// <returns>y</returns>
+            public ComplexNumber Eval(double x)
+            {
+                var y = Eval(new ComplexNumber() { Real = x, Imaginary = 0 });
+                return y;
+            }
+
+            /// <summary>
+            /// Evaluates polynomial at given point
+            /// </summary>
+            /// <param name="x">point of evaluation</param>
+            /// <returns>y</returns>
+            public ComplexNumber Eval(ComplexNumber x)
+            {
+                ComplexNumber result = ComplexNumber.Zero;
+                ComplexNumber xPower = new ComplexNumber() { Real = 1, Imaginary = 0 };
+                for (int i = 0; i < Coefficients.Count; i++)
+                {
+                    //ComplexNumber coef = Coefficients[i];
+                    //ComplexNumber bx = x;
+                    //int power = i;
+
+                    //if (i > 0)
+                    //{
+                    //    for (int j = 0; j < power - 1; j++)
+                    //        bx = bx.Multiply(x);
+
+                    //    coef = coef.Multiply(bx);
+                    //}
+
+                    //s = s.Add(coef);
+                    result = result.Add(Coefficients[i].Multiply(xPower));
+                    xPower = xPower.Multiply(x); // Calculate x^(i+1) for the next iteration
+                }
+                return result;
+            }
+
+            /// <summary>
+            /// ToString
+            /// </summary>
+            /// <returns>String repr of polynomial</returns>
+            public override string ToString()
+            {
+                string s = "";
+                int i = 0;
+                for (; i < Coefficients.Count; i++)
+                {
+                    s += Coefficients[i];
+                    if (i > 0)
+                    {
+                        int j = 0;
+                        for (; j < i; j++)
+                        {
+                            s += "x";
+                        }
+                    }
+                    if (i+1<Coefficients.Count)
+                    s += " + ";
+                }
+                return s;
+            }
+        }
+    }
+}
