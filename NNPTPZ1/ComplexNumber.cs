76b0b492e5d9012cf77e6ed507b30048cb88ca37
Kopec9 <kopecmar69@gmail.com>
2023-10-13 22:54:34 +0200

    Přejmenování atributů, proměnných, parametrů, třídy a souboru v ComplexNumber.cs

---
+++ NNPTPZ1/ComplexNumber.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace NNPTPZ1
+{
+
+    namespace Mathematics
+    {
+        public class ComplexNumber
+        {
+            public double RealElement { get; set; }
+            public double ImaginaryElement { get; set; }
+
+            public readonly static ComplexNumber Zero = new ComplexNumber()
+            {
+                RealElement = 0,
+                ImaginaryElement = 0
+            };            
+            
+            public ComplexNumber Add(ComplexNumber leftAddend)
+            {
+                ComplexNumber rightAddend = this;
+                return new ComplexNumber()
+                {
+                    RealElement = rightAddend.RealElement + leftAddend.RealElement,
+                    ImaginaryElement = rightAddend.ImaginaryElement + leftAddend.ImaginaryElement
+                };
+            }
+
+            public ComplexNumber Subtract(ComplexNumber subtrahend)
+            {
+                ComplexNumber minuend = this;
+                return new ComplexNumber()
+                {
+                    RealElement = minuend.RealElement - subtrahend.RealElement,
+                    ImaginaryElement = minuend.ImaginaryElement - subtrahend.ImaginaryElement
+                };
+            }
+            public ComplexNumber Multiply(ComplexNumber rightFactor)
+            {
+                ComplexNumber leftFactor = this;
+                // aRe*bRe + aRe*bIm*i + aIm*bRe*i + aIm*bIm*i*i
+                return new ComplexNumber()
+                {
+                    RealElement = (leftFactor.RealElement * rightFactor.RealElement) - (leftFactor.ImaginaryElement * rightFactor.ImaginaryElement),
+                    ImaginaryElement = leftFactor.RealElement * rightFactor.ImaginaryElement + leftFactor.ImaginaryElement * rightFactor.RealElement
+                };
+            }             
+            internal ComplexNumber Divide(ComplexNumber complexNumber)
+            {
+                // (aRe + aIm*i) / (bRe + bIm*i)
+                // ((aRe + aIm*i) * (bRe - bIm*i)) / ((bRe + bIm*i) * (bRe - bIm*i))
+                //  bRe*bRe - bIm*bIm*i*i
+                var divident = this.Multiply(new ComplexNumber() { RealElement = complexNumber.RealElement, ImaginaryElement = -complexNumber.ImaginaryElement });
+                var divisor = (complexNumber.RealElement * complexNumber.RealElement) + (complexNumber.ImaginaryElement * complexNumber.ImaginaryElement);
+
+                return new ComplexNumber()
+                {
+                    RealElement = divident.RealElement / divisor,
+                    ImaginaryElement = divident.ImaginaryElement / divisor
+                };
+            }            
+            public override bool Equals(object inputObject)
+            {
+                if (inputObject is ComplexNumber)
+                {
+                    ComplexNumber complexNumberToCompare = inputObject as ComplexNumber;
+                    return complexNumberToCompare.RealElement == RealElement && complexNumberToCompare.ImaginaryElement == ImaginaryElement;
+                }
+                return base.Equals(inputObject);
+            }
+
+            public double GetAbS()
+            {
+                return Math.Sqrt((RealElement * RealElement) + (ImaginaryElement * ImaginaryElement));
+            }
+
+            public double GetAngleInRadians()
+            {
+                return Math.Atan(ImaginaryElement / RealElement);
+            }
+
+            public override string ToString()
+            {
+                return $"({RealElement} + {ImaginaryElement}i)";
+            }
+        }
+    }
+}
