5c946ac883e521a108be98168e747d2c35848da4
Kopec9 <kopecmar69@gmail.com>
2023-10-17 22:46:07 +0200

    Vytvoření metody FindBitmapCoordinatesOfPixel, vytvoření enumu, přejmenování části proměnných

--- NNPTPZ1/Program.cs
+++ NNPTPZ1/Program.cs
@@ -24,36 +24,34 @@ namespace NNPTPZ1
     class Program
     {
         static string fileName;
-        static int[] bitmapDimensions = new int[2];
+        static int[] bitmapDimensions;
+        static double[] axisLimits;
         static Bitmap bitmap;
         static List<ComplexNumber> roots;
 
         static void Main(string[] args)
         {
-            
+            bitmapDimensions = new int[2];
+            axisLimits = new double[4];
+            ComplexNumber pixelWithCoordinates = new ComplexNumber();
+            roots = new List<ComplexNumber>();
+            bitmap = new Bitmap(bitmapDimensions[0], bitmapDimensions[1]);
+
             for (int i = 0; i < bitmapDimensions.Length; i++)
             {
                 bitmapDimensions[i] = int.Parse(args[i]);
             }
-            double[] doubleargs = new double[4];
-            for (int i = 0; i < doubleargs.Length; i++)
+
+            for (int i = 0; i < axisLimits.Length; i++)
             {
-                doubleargs[i] = double.Parse(args[i + 2]);
+                axisLimits[i] = double.Parse(args[i + 2]);
             }
             fileName = args[6];
             // TODO: add parameters from args?
-            
-            double xmin = doubleargs[0];
-            double xmax = doubleargs[1];
-            double ymin = doubleargs[2];
-            double ymax = doubleargs[3];
 
-            double xstep = (xmax - xmin) / bitmapDimensions[0];
-            double ystep = (ymax - ymin) / bitmapDimensions[1];
 
             //List<ComplexNumber> roots = new List<ComplexNumber>();
-            roots = new List<ComplexNumber>();
-            bitmap = new Bitmap(bitmapDimensions[0], bitmapDimensions[1]);
+
             // TODO: poly should be parameterised?
             Polynomial polynomial = new Polynomial();
             polynomial.ListOfComplexNumbers.Add(new ComplexNumber() { RealElement = 1 });
@@ -81,19 +79,7 @@ namespace NNPTPZ1
                 for (int j = 0; j < bitmapDimensions[1]; j++)
                 {
                     // find "world" coordinates of pixel
-                    double y = ymin + i * ystep;
-                    double x = xmin + j * xstep;
-
-                    ComplexNumber ox = new ComplexNumber()
-                    {
-                        RealElement = x,
-                        ImaginaryElement = (float)(y)
-                    };
-
-                    if (ox.RealElement == 0)
-                        ox.RealElement = 0.0001;
-                    if (ox.ImaginaryElement == 0)
-                        ox.ImaginaryElement = 0.0001f;
+                    pixelWithCoordinates = FindBitmapCoordinatesOfPixel(i, j);
 
                     //Console.WriteLine(ox);
 
@@ -101,8 +87,8 @@ namespace NNPTPZ1
                     float it = 0;
                     for (int q = 0; q < 30; q++)
                     {
-                        var diff = polynomial.Eval(ox).Divide(derivedPolynomial.Eval(ox));
-                        ox = ox.Subtract(diff);
+                        var diff = polynomial.Eval(pixelWithCoordinates).Divide(derivedPolynomial.Eval(pixelWithCoordinates));
+                        pixelWithCoordinates = pixelWithCoordinates.Subtract(diff);
 
                         //Console.WriteLine($"{q} {ox} -({diff})");
                         if (Math.Pow(diff.RealElement, 2) + Math.Pow(diff.ImaginaryElement, 2) >= 0.5)
@@ -115,7 +101,7 @@ namespace NNPTPZ1
                     //Console.ReadKey();
 
                     // find solution root number
-                    int rootNumber = getSolutionRootNumber(ox);
+                    int rootNumber = getSolutionRootNumber(pixelWithCoordinates);
 
                     // colorize pixel according to root number
                     //int vv = id;
@@ -139,19 +125,49 @@ namespace NNPTPZ1
             //}
 
 
-            saveIntoFile();
+            SaveIntoFile();
             //bitmap.Save(fileName ?? "../../../out.png");
 
             //Console.ReadKey();
         }
 
-        private static int getSolutionRootNumber(ComplexNumber ox)
+        private static ComplexNumber FindBitmapCoordinatesOfPixel(int i, int j)
+        {
+
+            double xmin = axisLimits[0];
+            double xmax = axisLimits[1];
+            double ymin = axisLimits[2];
+            double ymax = axisLimits[3];
+
+            double xstep = (xmax - xmin) / bitmapDimensions[0];
+            double ystep = (ymax - ymin) / bitmapDimensions[1];
+
+            ComplexNumber pixelWithCoordinates;
+
+            double y = ymin + i * ystep;
+            double x = xmin + j * xstep;
+
+            pixelWithCoordinates = new ComplexNumber()
+            {
+                RealElement = x,
+                ImaginaryElement = (y)
+            };
+
+            if (pixelWithCoordinates.RealElement == 0)
+                pixelWithCoordinates.RealElement = 0.0001;
+            if (pixelWithCoordinates.ImaginaryElement == 0)
+                pixelWithCoordinates.ImaginaryElement = 0.0001f;
+
+            return pixelWithCoordinates;
+        }
+
+        private static int getSolutionRootNumber(ComplexNumber pixelWithCoordinates)
         {
             var rootNumber = 0;
             var solutionFound = false;
             for (int i = 0; i < roots.Count; i++)
             {
-                if (Math.Pow(ox.RealElement - roots[i].RealElement, 2) + Math.Pow(ox.ImaginaryElement - roots[i].ImaginaryElement, 2) <= 0.01)
+                if (Math.Pow(pixelWithCoordinates.RealElement - roots[i].RealElement, 2) + Math.Pow(pixelWithCoordinates.ImaginaryElement - roots[i].ImaginaryElement, 2) <= 0.01)
                 {
                     solutionFound = true;
                     rootNumber = i;
@@ -159,7 +175,7 @@ namespace NNPTPZ1
             }
             if (!solutionFound)
             {
-                roots.Add(ox);
+                roots.Add(pixelWithCoordinates);
                 rootNumber = roots.Count;
                 //maxId = id + 1;
             }
@@ -169,16 +185,29 @@ namespace NNPTPZ1
 
         public static Color CalculatePixelColorAccordingToRootNumber(Color[] colors, float it, int rootNumber)
         {
+            //Color pixelColor = Color.FromName(((Colors) (rootNumber % Enum.GetNames(typeof(Colors)).Length)).ToString());
             Color pixelColor = colors[rootNumber % colors.Length];
             pixelColor = Color.FromArgb(pixelColor.R, pixelColor.G, pixelColor.B);
             pixelColor = Color.FromArgb(Math.Min(Math.Max(0, pixelColor.R - (int)it * 2), 255), Math.Min(Math.Max(0, pixelColor.G - (int)it * 2), 255), Math.Min(Math.Max(0, pixelColor.B - (int)it * 2), 255));
             return pixelColor;
         }
 
-        public static void saveIntoFile()
+        public static void SaveIntoFile()
         {
             bitmap.Save(fileName ?? "../../../out.png");
         }
 
+
+        public enum Colors { 
+            Red, 
+            Blue,
+            Green, 
+            Yellow,
+            Orange, 
+            Fuchsia, 
+            Gold, 
+            Cyan, 
+            Magenta     
+        }
     }
 }
